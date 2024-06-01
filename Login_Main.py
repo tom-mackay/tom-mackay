b34a74de72d43141163c568f606da2f46ead71b2
james-coding-world <james.ludwig.a@gmail.com>
2024-06-01 13:02:45 -0600

    Developing login window.

--- Login_Main.py
+++ Login_Main.py
@@ -1,19 +1,35 @@
 import ttkbootstrap as ttk
 
-def login_gui_screen():
-    
-    # Create the main application window
-    root = ttk.Window(themename="vapor")
-    root.title("Login Screen")  # Set the screen name
-    root.geometry("400x300")    # Set the screen geometry (width x height)
 
-    # Start the application
-    root.mainloop()
+#! LoginScreen 
+class LoginScreen(ttk.Frame):
+    def __init__(self, loginmaster=None):
+        super().__init__(loginmaster, themename="vapor")
+        
+        self.login_screen = loginmaster
+        self.login_screen.title("Initialize User")  # Set the screen name
+        self.login_screen.geometry("600x600")    # Set the screen geometry (width x height)
+        self.pack()
+
+        #! Fonts Variables
+        self.title_font = ("Helvetica", 16)
+
+        #! Create a spacer (separator)
+        self.spacer = ttk.Separator(self, orient="horizontal")
+        self.spacer.pack(fill="x", pady=10)  # Add padding around the spacer
+
+        #! Create Title Label
+        self.login_title_lbl = ttk.Label(self, text="Welcome Petty Fool", font=self.title_font)
+        self.login_title_lbl.pack(pady=10)
+
+
+
+
 
 if __name__ == "__main__":
-    login_gui_screen()
-    
-    
+    root = ttk.Window()
+    app = LoginScreen(master=root)
+    app.mainloop()
     
     
     
@@ -47,10 +63,6 @@ if __name__ == "__main__":
 #     selected_texts = [listbox.get(i) for i in selected_items]
 #     print("Selected items:", selected_texts)
 
-#     # Create and pack a label
-#     label = ttk.Label(root, text="This is a Label", font=("Helvetica", 16))
-#     label.pack(pady=10)
-
 #     # Create and pack an entry (text input)
 #     entry = ttk.Entry(root, font=("Helvetica", 16))
 #     entry.pack(pady=10)
@@ -59,18 +71,23 @@ if __name__ == "__main__":
 #     button = ttk.Button(root, text="Click Me", command=on_button_click, bootstyle=PRIMARY)
 #     button.pack(pady=10)
 
+
+
+
+
+
+
+
+
+# Listbox Details
 #     # Create and pack a listbox with a scrollbar
 #     listbox_frame = ttk.Frame(root)
 #     listbox_frame.pack(pady=10)
-
 #     listbox_scrollbar = ttk.Scrollbar(listbox_frame)
 #     listbox_scrollbar.pack(side=RIGHT, fill=Y)
-
 #     listbox = ttk.Listbox(listbox_frame, yscrollcommand=listbox_scrollbar.set, font=("Helvetica", 16))
 #     listbox.pack()
-
 #     listbox_scrollbar.config(command=listbox.yview)
-
 #     # Add items to the listbox
 #     for item in ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]:
 #         listbox.insert(END, item)
