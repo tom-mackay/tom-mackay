64020ee4e79dde53f7abc9cefc11eebe4317bc7e
james-coding-world <james.ludwig.a@gmail.com>
2024-06-01 23:26:48 -0600

    Add logging function and add logs_folder to gitgnore.

--- Login_Main.py
+++ Login_Main.py
@@ -2,113 +2,132 @@ import tkinter as tk
 import ttkbootstrap as ttk
 import os
 import logging
+from datetime import datetime
 from Login_Validate import validate_login_func
 
-if os.access('output.log', os.W_OK):
-    logging.basicConfig(filename="output.log", level=logging.DEBUG)
-
-    logging.debug('This is a debug message')
-    logging.info('This is an informational message')
-    logging.warning('This is a warning message')
-    logging.error('This is an error message')
-    logging.critical('This is a critical error message')
+# if os.access('output.log', os.W_OK):
+#     logging.basicConfig(filename="system_log_file.log", level=logging.DEBUG)
+#     logging.debug('This is a debug message')
+#     logging.info('This is an informational message')
+#     logging.warning('This is a warning message')
+#     logging.error('This is an error message')
+#     logging.critical('This is a critical error message')
+    
+    
 class LoginScreen(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         
-        self.relative_path = os.path.abspath(__file__)
-        print(self.relative_path)
+        #! Initializing Logging
+        initialize_Logging()
+        initializeUI(self)
         
-        #! Screen Setup
-        self.main_login_screen = master
-        self.main_login_screen.title("Initialize User")
         
-        #! Get screen width and height
-        screen_width = self.main_login_screen.winfo_screenwidth()
-        screen_height = self.main_login_screen.winfo_screenheight()
-        #! Calculate x and y positions for the window to be centered
-        x = (screen_width - 600) // 2  # 600 is the window width
-        y = (screen_height - 230) // 2  # 230 is the window height
-        #! Set the window position
-        #self.main_login_screen.geometry(f"600x230+{x}+{y}")
-        self.main_login_screen.geometry("600x230") #^ WidthxHeight
-        self.main_login_screen.resizable(False, False) 
-        self.pack()
-        #self.grid(row=0, column=0) #& Review another time
+        def initialize_Logging():
+            # Generate the timestamp
+            timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+            log_filename = f"system_log_file_{timestamp}.log"
+            # Configure logging
+            logging.basicConfig(level=logging.DEBUG,
+                                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                handlers=[
+                                    logging.FileHandler(log_filename),
+                                    logging.StreamHandler()
+                                ])
+                        
         
-        #! Fonts
-        self.title_font = ("Helvetica", 16)
-        self.input_font = ("Helvetica", 12)
- 
-        #! Create a Login Canvas widget
-        self.login_canvas = tk.Canvas(self)
-        self.login_canvas.pack(fill=tk.BOTH, expand=True)
         
-        # Spacer
-        self.spacer = ttk.Label(self.login_canvas, text="")
-        self.spacer.pack()
+        def initializeUI(self):
+            self.relative_path = os.path.abspath(__file__)
+            print(self.relative_path)
+            
+            #! Screen Setup
+            self.main_login_screen = master
+            self.main_login_screen.title("Initialize User")
+            
+            #! Get screen width and height
+            screen_width = self.main_login_screen.winfo_screenwidth()
+            screen_height = self.main_login_screen.winfo_screenheight()
+            #! Calculate x and y positions for the window to be centered
+            x = (screen_width - 600) // 2  # 600 is the window width
+            y = (screen_height - 230) // 2  # 230 is the window height
+            #! Set the window position
+            #self.main_login_screen.geometry(f"600x230+{x}+{y}")
+            self.main_login_screen.geometry("600x230") #^ WidthxHeight
+            self.main_login_screen.resizable(False, False) 
+            self.pack()
+            #self.grid(row=0, column=0) #& Review another time
+            
+            #! Fonts
+            self.title_font = ("Helvetica", 16)
+            self.input_font = ("Helvetica", 12)
+    
+            #! Create a Login Canvas widget
+            self.login_canvas = tk.Canvas(self)
+            self.login_canvas.pack(fill=tk.BOTH, expand=True)
+            
+            # Spacer
+            self.spacer = ttk.Label(self.login_canvas, text="")
+            self.spacer.pack()
 
-        #! Login Title Label
-        self.login_title_lbl = ttk.Label(self.login_canvas, text="Welcome PETTY Fool", font=self.title_font)
-        self.login_title_lbl.pack(anchor=tk.CENTER)
-        
-        # Spacer
-        self.spacer2 = ttk.Label(self.login_canvas, text="")
-        self.spacer2.pack()
-        
-        #! Main Login Frame
-        self.login_frame = ttk.Frame(self.login_canvas)
-        self.login_frame.pack(anchor=tk.CENTER)
-        
-        #! Username Input and Label
-        self.username_lbl = ttk.Label(self.login_frame, text="Username:", font=self.input_font)
-        self.username_lbl.grid(padx=5, row=0, column=0)
-        self.username_input = ttk.Entry(self.login_frame, font=self.input_font)
-        self.username_input.grid(padx=5, row=0, column=1)
-        # Spacer
-        self.spacer3 = ttk.Label(self.login_frame, text="")
-        self.spacer3.grid(row=1, column=0)
-        #! Password Input and Label
-        self.password_lbl = ttk.Label(self.login_frame, text="Password:", font=self.input_font)
-        self.password_lbl.grid(padx=5, row=2, column=0)
-        self.password_input = ttk.Entry(self.login_frame, font=self.input_font, show="*")
-        self.password_input.grid(padx=5, row=2, column=1)
+            #! Login Title Label
+            self.login_title_lbl = ttk.Label(self.login_canvas, text="Welcome PETTY Fool", font=self.title_font)
+            self.login_title_lbl.pack(anchor=tk.CENTER)
+            
+            # Spacer
+            self.spacer2 = ttk.Label(self.login_canvas, text="")
+            self.spacer2.pack()
+            
+            #! Main Login Frame
+            self.login_frame = ttk.Frame(self.login_canvas)
+            self.login_frame.pack(anchor=tk.CENTER)
+            
+            #! Username Input and Label
+            self.username_lbl = ttk.Label(self.login_frame, text="Username:", font=self.input_font)
+            self.username_lbl.grid(padx=5, row=0, column=0)
+            self.username_input = ttk.Entry(self.login_frame, font=self.input_font)
+            self.username_input.grid(padx=5, row=0, column=1)
+            # Spacer
+            self.spacer3 = ttk.Label(self.login_frame, text="")
+            self.spacer3.grid(row=1, column=0)
+            #! Password Input and Label
+            self.password_lbl = ttk.Label(self.login_frame, text="Password:", font=self.input_font)
+            self.password_lbl.grid(padx=5, row=2, column=0)
+            self.password_input = ttk.Entry(self.login_frame, font=self.input_font, show="*")
+            self.password_input.grid(padx=5, row=2, column=1)
 
-        # Spacer
-        self.spacer4 = ttk.Label(self.main_login_screen, text="")
-        self.spacer4.pack()
+            # Spacer
+            self.spacer4 = ttk.Label(self.main_login_screen, text="")
+            self.spacer4.pack()
 
-        #! Frame for Buttons
-        self.login_buttons_frame = ttk.Frame(self.main_login_screen)
-        self.login_buttons_frame.pack(anchor=tk.CENTER)
-        #! Login Button
-        self.login_button = ttk.Button(self.login_buttons_frame, text="Login", command=self.login_action, takefocus=False, style='Primary.TButton')
-        self.login_button.grid(padx=20, row=0, column=1)
-        #! Info Button
-        self.logininfo_button = ttk.Button(self.login_buttons_frame, text="Info", command=self.info_action, takefocus=False, style='Secondary.TButton')
-        self.logininfo_button.grid(padx=20, row=0, column=0)
+            #! Frame for Buttons
+            self.login_buttons_frame = ttk.Frame(self.main_login_screen)
+            self.login_buttons_frame.pack(anchor=tk.CENTER)
+            #! Login Button
+            self.login_button = ttk.Button(self.login_buttons_frame, text="Login", command=self.login_action, takefocus=False, style='Primary.TButton')
+            self.login_button.grid(padx=20, row=0, column=1)
+            #! Info Button
+            self.logininfo_button = ttk.Button(self.login_buttons_frame, text="Info", command=self.info_action, takefocus=False, style='Secondary.TButton')
+            self.logininfo_button.grid(padx=20, row=0, column=0)
 
-        # Spacer
-        self.spacer5 = ttk.Label(self.main_login_screen, text="")
-        self.spacer5.pack()
+            # Spacer
+            self.spacer5 = ttk.Label(self.main_login_screen, text="")
+            self.spacer5.pack()
 
-        #? Frame for Signup Buttons 
-        self.signup_button_frame = ttk.Frame(self.main_login_screen)
-        self.signup_button_frame.pack(anchor="se")          #? Aight so turns out this anchor uses cardinal directions fyi 
+            #? Frame for Signup Buttons 
+            self.signup_button_frame = ttk.Frame(self.main_login_screen)
+            self.signup_button_frame.pack(anchor="se")          #? Aight so turns out this anchor uses cardinal directions fyi 
 
-        #& Signup Button
-        self.signup_button = ttk.Button(self.signup_button_frame, text="¿ Que, no bitches ?", takefocus=False, style='info.Link.TButton')
-        self.signup_button.grid(padx=0, pady=0, row=1, column=0)
-
-        self.signup_lbl = ttk.Label(self.signup_button_frame, text="¿ Que, no bitches ?", takefocus=False)
-        self.signup_lbl.grid(padx=0, pady=0, row=1, column=0)   
-
-        # Spacer
-        self.spacer6 = ttk.Label(self.main_login_screen, text="")
-        self.spacer6.pack(anchor="se", padx=0, pady=10)
-
-         # Spacer
-        #& self.signup_button = ttk.Button(self.login_canvas, text="Sign Up", takefocus=True, style='info.Link.TButton')
+            #& Signup Button
+            # self.signup_button = ttk.Button(self.signup_button_frame, text="¿ Que, no bitches ?", takefocus=False, style='info.Link.TButton')
+            # self.signup_button.grid(padx=0, pady=0, row=1, column=0)
+            # self.signup_lbl = ttk.Label(self.signup_button_frame, text="¿ Que, no bitches ?", takefocus=False)
+            # self.signup_lbl.grid(padx=0, pady=0, row=1, column=0)   
+            # # Spacer
+            # self.spacer6 = ttk.Label(self.main_login_screen, text="")
+            # self.spacer6.pack(anchor="se", padx=0, pady=10)
+            # Spacer
+            #& self.signup_button = ttk.Button(self.login_canvas, text="Sign Up", takefocus=True, style='info.Link.TButton')
              
              
              
@@ -131,17 +150,6 @@ class LoginScreen(ttk.Frame):
             print("Login Successful")
         else:
             print("Login Failed, Check Credentials")
-
-
-
-
-
-
-
-
-
-
-
 
     def info_action(self):
         # Define the action to be taken when the info button is clicked
